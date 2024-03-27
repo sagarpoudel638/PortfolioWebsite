666345f9a4765f84d0a4c3730b78d8509019b1b4
Sagar Poudel <46084070+sagarpoudel638@users.noreply.github.com>
2024-03-27 22:09:55 +1100

    initial commit

---
+++ javascript/javascript.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {myFunction()};
+
+var navigation = document.getElementById("navigation");
+var sticky = navigation.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navigation.classList.add("sticky")
+  } else {
+    navigation.classList.remove("sticky");
+  }
+}
+
